fd256cebb0eda04a0b4855b15b264be5f565071f
noor1234-cloud <ainn4931@gmail.com>
2024-12-28 18:12:20 +0500

    Add files via upload

--- books.dart
+++ books.dart
@@ -1,60 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'dart:convert';
+import '../models/books.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-
-BooksModel booksModelFromJson(String str) => BooksModel.fromJson(json.decode(str));
-
-String booksModelToJson(BooksModel data) => json.encode(data.toJson());
-
-class BooksModel {
-  final String? docId;
-  final String? catId;
-  final String? userId;
-  final String? name;
-  final String? totalPages;
-  final String? authorName;
-  final String? imageUrl;
-  final String? stock;
-  final String? createdAt;
-
-  BooksModel({
-    this.docId,
-    this.catId,
-    this.userId,
-    this.name,
-    this.totalPages,
-    this.authorName,
-    this.imageUrl,
-    this.stock,
-    this.createdAt,
-  });
-
-  factory BooksModel.fromJson(Map<String, dynamic> json) =>
-      BooksModel(
-        docId: json["docId"],
-        catId: json["catID"],
-        userId: json["userID"],
-        name: json["name"],
-        totalPages: json["totalPages"],
-        authorName: json["authorName"],
-        imageUrl: json["image"],
-        stock: json["stock"],
-        createdAt: json["createdAt"],
-      );
-
-  Map<String, dynamic> toJson({String? docId}) {
-    return {
-      "docId": docId ?? this.docId,
-      "catID": catId,
-      "userID": userId,
-      "name": name,
-      "totalPages": totalPages,
-      "authorName": authorName,
-      "image": imageUrl,
-      "stock": stock,
-      "createdAt": createdAt,
-    };
+class BooksServices {
+  /// Create Book
+  Future createBook(BooksModel model) async {
+    DocumentReference documentReference =
+    FirebaseFirestore.instance.collection('BooksCollection').doc();
+    await documentReference.set(model.toJson(docId: documentReference.id));
   }
 
+  /// Get All Books
+  Stream<List<BooksModel>> getAllBooks() {
+    return FirebaseFirestore.instance
+        .collection('BooksCollection')
+        .where('stock', isGreaterThan: 0)
+        .snapshots()
+        .map((booksList) => booksList.docs
+        .map((book) => BooksModel.fromJson(book.data()..['docId'] = book.id))
+        .toList());
+  }
+
+  /// Get Out-of-Stock Books
+  Stream<List<BooksModel>> getOutOfStockBooks() {
+    return FirebaseFirestore.instance
+        .collection('BooksCollection')
+        .where('stock', isEqualTo: 0)
+        .snapshots()
+        .map((booksList) => booksList.docs
+        .map((book) => BooksModel.fromJson(book.data()..['docId'] = book.id))
+        .toList());
+  }
+
+  /// Get My Books
+  Stream<List<BooksModel>> getMyBooks(String userId) {
+    value: BooksServices().getMyBooks(userId);
+    return FirebaseFirestore.instance
+        .collection('BooksCollection')
+        .where('userId', isEqualTo: userId)
+        .snapshots()
+        .map((booksList) => booksList.docs
+        .map((book) => BooksModel.fromJson(book.data()..['docId'] = book.id)) // Add docId
+        .toList());
+  }
+
+  /// Update Book
+  Future updateBook(BooksModel model) async {
+    await FirebaseFirestore.instance
+        .collection('BooksCollection')
+        .doc(model.docId)
+        .update({
+      "name": model.name,
+      "authorName": model.authorName,
+      "stock": model.stock,
+    });
+  }
+
+  /// Delete Book
+  Future deleteBook(String docId) async {
+    await FirebaseFirestore.instance
+        .collection('BooksCollection')
+        .doc(docId)
+        .delete();
+  }
 }
